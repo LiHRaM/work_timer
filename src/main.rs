6e3275ed7d7865c1ae379f43519cc241f85252c1
Hilmar Gústafsson <hilmargustafs@gmail.com>
2018-03-15 10:45:58 +0100

    Initial work on CMD functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#[macro_use]
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "The Work Timer", about="A time-tracking tool for people who do stuff.")]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Option<Command>
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    #[structopt(name="punch-in",
+                about="Register the beginning of a work-period.")]
+    PunchIn,
+    #[structopt(name="punch-out",
+                about="Register the end of a work-period.")]
+    PunchOut
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+
+    match opt.cmd {
+        Some(Command::PunchOut) => {
+            println!("Punching out...");
+        },
+        Some(Command::PunchIn) => {
+            println!("Punching in...");
+        },
+        _ => println!("What are you doing, man!?")
+    }
 }
